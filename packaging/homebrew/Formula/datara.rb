0fc3ea4f6e8423566ac3f855fb376b9855ee4876
waters1ze <Kirillperekatnov13@gmail.com>
2026-09-17 01:36:26 +0300

    chore(packaging): sync package manifests with fresh v1.4.1 release SHA-256 checksums

--- packaging/homebrew/Formula/datara.rb
+++ packaging/homebrew/Formula/datara.rb
@@ -7,16 +7,16 @@ class Datara < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/datara-lang/datara/releases/download/v1.4.1/forgen-darwin-arm64.tar.gz"
-      sha256 "96d587f3f796dc6ac7fddb61d0e85bf09103640cecb23505218a425b5fad4e97"
+      sha256 "fd30408c78a9eeeaa90a23b502936953753b87684ae34c5ab1e099b8ba9c2e67"
     else
       url "https://github.com/datara-lang/datara/releases/download/v1.4.1/forgen-darwin-x64.tar.gz"
-      sha256 "9f54b96a56a6d98501a7c98c73cf23b6df55d4f0586bce0134d7a4a4f81c5e42"
+      sha256 "b19ca68f70e66f73a1640ab54c2e3776645d78c9c123c10e9bd838ba4d8a7e87"
     end
   end
 
   on_linux do
     url "https://github.com/datara-lang/datara/releases/download/v1.4.1/forgen-linux-x64.tar.gz"
-    sha256 "7a3940385388ad7868161f61a5cb6a1b412b66df07c24536b1da2886a807ace5"
+    sha256 "c472b8942be0d8982b44cb92ee16694387b9f065e34e1110d07024f687d1d7d4"
   end
 
   def install
